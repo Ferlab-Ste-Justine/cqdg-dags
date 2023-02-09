cfbfa6aec039db85779c41dedc056494afc6c683
adrian <adipaul1981@yahoo.ca>
2023-02-09 16:13:21 -0500

    update etl.py

--- dags/etl.py
+++ dags/etl.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from airflow import DAG
 from airflow.models.param import Param
 
+from lib import config
 from lib.config import env, Env, K8sContext
 from lib.operators.spark import SparkOperator
 
