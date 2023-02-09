2c8144563f2befc11e75db4576a2e2c357964d35
adrian <adipaul1981@yahoo.ca>
2023-02-09 16:25:39 -0500

    test

--- dags/etl.py
+++ dags/etl.py
@@ -3,8 +3,8 @@ from datetime import datetime
 from airflow import DAG
 from airflow.models.param import Param
 
-from config import env, Env, K8sContext
-from operators.spark import SparkOperator
+from lib.config import env, Env, K8sContext
+from lib.operators.spark import SparkOperator
 
 if env in [Env.QA, Env.DEV]:
 
