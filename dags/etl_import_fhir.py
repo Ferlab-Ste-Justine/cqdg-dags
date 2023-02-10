acc449621c7fb895013f967f8ead734791d4d59c
adrian <adipaul1981@yahoo.ca>
2023-02-10 17:13:41 -0500

    delete IG

--- dags/etl_import_fhir.py
+++ dags/etl_import_fhir.py
@@ -1,6 +1,9 @@
 from datetime import datetime
+
 from airflow import DAG
-from airflow.models.param import Param
+
+from dags.lib.operators.fhir import FhirOperator
+from dags.lib.operators.wait import WaitOperator
 from lib.config import K8sContext
 from lib.operators.fhir_import import FhirCsvOperator
 
@@ -11,6 +14,17 @@ with DAG(
     params={},
 ) as dag:
 
+    ig_publish = FhirOperator(
+        task_id='ig_publish',
+        name='etl-import-fhir-ig-publish',
+        k8s_context=K8sContext.DEFAULT,
+    )
+
+    wait_30s = WaitOperator(
+        task_id='wait_30s',
+        time='30s',
+    )
+
     csv_import = FhirCsvOperator(
         task_id='fhir_import',
         name='etl-fhir_import',
@@ -18,4 +32,4 @@ with DAG(
         arguments=["-cp", "cqdg-fhir-import.jar", "bio/ferlab/cqdg/etl/FhirImport", "clinical-data/e2adb961-4f58-4e13-a24f-6725df802e2c", "cqdg-qa-app-clinical-data-service", "7", "13", "cag", "true", "1615,1616,1617,1644,1645,1646,1647,1650,1651,1656,1658,1659,1660,1661,1667,1668,1669,1680,1681,1682,1690,1691,1692,1704,1707,1735,1736,1737,1738,1748,1750,1854,1855,1856,1857,1860,1861,1862,1865,1866,1867,1872,1873,1876,1877,1881,1882,1884,1885,1886,1887,1894,1895,1898,1899,1902,1903,1906,1907,1916,1917,1918,1919,1920,1921,1922,1924,1925,1926,1927,1928,1929,1930,1934,1935,1951,1952,A00516_0270,A00516_0271"],
     )
 
-    # ig_publish >> wait_30s >> csv_import
+    ig_publish >> wait_30s >> csv_import
---
+++ dags/lib/operators/fhir.py
@@ -0,0 +1,68 @@
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from kubernetes.client import models as k8s
+from lib import config
+from lib.config import env_url
+from lib.utils import join
+
+
+class FhirOperator(KubernetesPodOperator):
+
+    template_fields = KubernetesPodOperator.template_fields + (
+        'color',
+    )
+
+    def __init__(
+        self,
+        k8s_context: str,
+        color: str = '',
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            is_delete_operator_pod=True,
+            in_cluster=config.k8s_in_cluster(k8s_context),
+            config_file=config.k8s_config_file(k8s_context),
+            cluster_context=config.k8s_cluster_context(k8s_context),
+            namespace=config.k8s_namespace,
+            image=config.fhir_image,
+            **kwargs,
+        )
+        self.color = color
+
+    def execute(self, **kwargs):
+
+        self.image_pull_secrets = [
+            k8s.V1LocalObjectReference(
+                name='images-registry-credentials',
+            ),
+        ]
+        self.env_vars = [
+            k8s.V1EnvVar(
+                name='BASE_URL',
+                value='https://' + join('-', ['fhir', self.color]) + env_url('.') +
+                '.cqgc.hsj.rtss.qc.ca/fhir',
+            ),
+            k8s.V1EnvVar(
+                name='OAUTH_URL',
+                value='https://auth' + env_url('.') +
+                '.cqgc.hsj.rtss.qc.ca/auth/realms/clin/protocol/openid-connect/token',
+            ),
+            k8s.V1EnvVar(
+                name='OAUTH_CLIENT_ID',
+                value='clin-system',
+            ),
+            k8s.V1EnvVar(
+                name='OAUTH_CLIENT_SECRET',
+                value_from=k8s.V1EnvVarSource(
+                    secret_key_ref=k8s.V1SecretKeySelector(
+                        name='keycloak-client-system-credentials',
+                        key='client-secret',
+                    ),
+                ),
+            ),
+            k8s.V1EnvVar(
+                name='OAUTH_UMA_AUDIENCE',
+                value='clin-acl',
+            ),
+        ]
+
+        super().execute(**kwargs)
