71c7769b5703cf7bbe4a12c24c0feb31aa87a925
adrian <adipaul1981@yahoo.ca>
2023-02-15 14:48:51 -0500

    test

--- dags/lib/operators/spark.py
+++ dags/lib/operators/spark.py
@@ -53,83 +53,83 @@ class SparkOperator(KubernetesPodOperator):
         #         name='images-registry-credentials',
         #     ),
         # ]
-        # self.env_vars = [
-        #     k8s.V1EnvVar(
-        #         name='SPARK_CLIENT_POD_NAME',
-        #         value_from=k8s.V1EnvVarSource(
-        #             field_ref=k8s.V1ObjectFieldSelector(
-        #                 field_path='metadata.name',
-        #             ),
-        #         ),
-        #     ),
-        #     k8s.V1EnvVar(
-        #         name='SPARK_JAR',
-        #         value='https://github.com/Ferlab-Ste-Justine/etl-cqdg-portal/releases/download/v1.0.1/index-task.jar',
-        #     ),
-        #     k8s.V1EnvVar(
-        #         name='SPARK_CLASS',
-        #         value='bio.ferlab.fhir.etl.IndexTask',
-        #     ),
-        # ]
-        # self.volumes = [
-        #     k8s.V1Volume(
-        #         name='spark-defaults',
-        #         config_map=k8s.V1ConfigMapVolumeSource(
-        #             name='spark-defaults',
-        #         ),
-        #     ),
-        #     k8s.V1Volume(
-        #         name='spark-s3-credentials',
-        #         secret=k8s.V1SecretVolumeSource(
-        #             secret_name='spark-s3-credentials',
-        #         ),
-        #     ),
-        # ]
-        # self.volume_mounts = [
-        #     k8s.V1VolumeMount(
-        #         name='spark-defaults',
-        #         mount_path='/opt/spark-configs/defaults',
-        #         read_only=True,
-        #     ),
-        #     k8s.V1VolumeMount(
-        #         name='spark-s3-credentials',
-        #         mount_path='/opt/spark-configs/s3-credentials',
-        #         read_only=True,
-        #     ),
-        # ]
-        #
-        # if self.spark_config:
-        #     self.volumes.append(
-        #         k8s.V1Volume(
-        #             name=self.spark_config,
-        #             config_map=k8s.V1ConfigMapVolumeSource(
-        #                 name=self.spark_config,
-        #             ),
-        #         ),
-        #     )
-        #     self.volume_mounts.append(
-        #         k8s.V1VolumeMount(
-        #             name=self.spark_config,
-        #             mount_path=f'/opt/spark-configs/{self.spark_config}',
-        #             read_only=True,
-        #         ),
-        #     )
-        # if self.spark_secret:
-        #     self.volumes.append(
-        #         k8s.V1Volume(
-        #             name=self.spark_secret,
-        #             secret=k8s.V1SecretVolumeSource(
-        #                 secret_name=self.spark_secret,
-        #             ),
-        #         ),
-        #     )
-        #     self.volume_mounts.append(
-        #         k8s.V1VolumeMount(
-        #             name=self.spark_secret,
-        #             mount_path=f'/opt/spark-configs/{self.spark_secret}',
-        #             read_only=True,
-        #         ),
-        #     )
+        self.env_vars = [
+            k8s.V1EnvVar(
+                name='SPARK_CLIENT_POD_NAME',
+                value_from=k8s.V1EnvVarSource(
+                    field_ref=k8s.V1ObjectFieldSelector(
+                        field_path='metadata.name',
+                    ),
+                ),
+            ),
+            k8s.V1EnvVar(
+                name='SPARK_JAR',
+                value='https://github.com/Ferlab-Ste-Justine/etl-cqdg-portal/releases/download/v1.0.1/index-task.jar',
+            ),
+            k8s.V1EnvVar(
+                name='SPARK_CLASS',
+                value='bio.ferlab.fhir.etl.IndexTask',
+            ),
+        ]
+        self.volumes = [
+            k8s.V1Volume(
+                name='spark-defaults',
+                config_map=k8s.V1ConfigMapVolumeSource(
+                    name='spark-defaults',
+                ),
+            ),
+            k8s.V1Volume(
+                name='spark-s3-credentials',
+                secret=k8s.V1SecretVolumeSource(
+                    secret_name='spark-s3-credentials',
+                ),
+            ),
+        ]
+        self.volume_mounts = [
+            k8s.V1VolumeMount(
+                name='spark-defaults',
+                mount_path='/opt/spark-configs/defaults',
+                read_only=True,
+            ),
+            k8s.V1VolumeMount(
+                name='spark-s3-credentials',
+                mount_path='/opt/spark-configs/s3-credentials',
+                read_only=True,
+            ),
+        ]
+
+        if self.spark_config:
+            self.volumes.append(
+                k8s.V1Volume(
+                    name=self.spark_config,
+                    config_map=k8s.V1ConfigMapVolumeSource(
+                        name=self.spark_config,
+                    ),
+                ),
+            )
+            self.volume_mounts.append(
+                k8s.V1VolumeMount(
+                    name=self.spark_config,
+                    mount_path=f'/opt/spark-configs/{self.spark_config}',
+                    read_only=True,
+                ),
+            )
+        if self.spark_secret:
+            self.volumes.append(
+                k8s.V1Volume(
+                    name=self.spark_secret,
+                    secret=k8s.V1SecretVolumeSource(
+                        secret_name=self.spark_secret,
+                    ),
+                ),
+            )
+            self.volume_mounts.append(
+                k8s.V1VolumeMount(
+                    name=self.spark_secret,
+                    mount_path=f'/opt/spark-configs/{self.spark_secret}',
+                    read_only=True,
+                ),
+            )
 
 
 
