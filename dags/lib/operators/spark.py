fea65c14686bd1909500e53b532c9836232f2214
adrian <adipaul1981@yahoo.ca>
2023-02-15 09:13:53 -0500

    fix

--- dags/lib/operators/spark.py
+++ dags/lib/operators/spark.py
@@ -53,14 +53,14 @@ class SparkOperator(KubernetesPodOperator):
             ),
         ]
         self.env_vars = [
-            # k8s.V1EnvVar(
-            #     name='SPARK_CLIENT_POD_NAME',
-            #     value_from=k8s.V1EnvVarSource(
-            #         field_ref=k8s.V1ObjectFieldSelector(
-            #             field_path='metadata.name',
-            #         ),
-            #     ),
-            # ),
+            k8s.V1EnvVar(
+                name='SPARK_CLIENT_POD_NAME',
+                value_from=k8s.V1EnvVarSource(
+                    field_ref=k8s.V1ObjectFieldSelector(
+                        field_path='metadata.name',
+                    ),
+                ),
+            ),
             k8s.V1EnvVar(
                 name='SPARK_JAR',
                 value=config.spark_jar,
@@ -77,12 +77,12 @@ class SparkOperator(KubernetesPodOperator):
                     name='spark-defaults',
                 ),
             ),
-            # k8s.V1Volume(
-            #     name='spark-s3-credentials',
-            #     secret=k8s.V1SecretVolumeSource(
-            #         secret_name='spark-s3-credentials',
-            #     ),
-            # ),
+            k8s.V1Volume(
+                name='spark-s3-credentials',
+                secret=k8s.V1SecretVolumeSource(
+                    secret_name='spark-s3-credentials',
+                ),
+            ),
         ]
         self.volume_mounts = [
             k8s.V1VolumeMount(
@@ -90,11 +90,11 @@ class SparkOperator(KubernetesPodOperator):
                 mount_path='/opt/spark-configs/defaults',
                 read_only=True,
             ),
-            # k8s.V1VolumeMount(
-            #     name='spark-s3-credentials',
-            #     mount_path='/opt/spark-configs/s3-credentials',
-            #     read_only=True,
-            # ),
+            k8s.V1VolumeMount(
+                name='spark-s3-credentials',
+                mount_path='/opt/spark-configs/s3-credentials',
+                read_only=True,
+            ),
         ]
 
         if self.spark_config:
@@ -133,40 +133,40 @@ class SparkOperator(KubernetesPodOperator):
         super().execute(**kwargs)
 
         config.k8s_load_config(self.k8s_context)
-        # k8s_client = kubernetes.client.CoreV1Api()
+        k8s_client = kubernetes.client.CoreV1Api()
 
         # Get driver pod log and delete driver pod
-        # driver_pod = k8s_client.list_namespaced_pod(
-        #     namespace=self.pod.metadata.namespace,
-        #     field_selector=f'metadata.name={self.pod.metadata.name}-driver',
-        #     limit=1,
-        # )
-        # if driver_pod.items:
-        #     log = k8s_client.read_namespaced_pod_log(
-        #         name=f'{self.pod.metadata.name}-driver',
-        #         namespace=self.pod.metadata.namespace,
-        #     )
-        #     logging.info(f'Spark job log:\n{log}')
-        #     k8s_client.delete_namespaced_pod(
-        #         name=f'{self.pod.metadata.name}-driver',
-        #         namespace=self.pod.metadata.namespace,
-        #     )
+        driver_pod = k8s_client.list_namespaced_pod(
+            namespace=self.pod.metadata.namespace,
+            field_selector=f'metadata.name={self.pod.metadata.name}-driver',
+            limit=1,
+        )
+        if driver_pod.items:
+            log = k8s_client.read_namespaced_pod_log(
+                name=f'{self.pod.metadata.name}-driver',
+                namespace=self.pod.metadata.namespace,
+            )
+            logging.info(f'Spark job log:\n{log}')
+            k8s_client.delete_namespaced_pod(
+                name=f'{self.pod.metadata.name}-driver',
+                namespace=self.pod.metadata.namespace,
+            )
 
         # Delete pod
-        # pod = k8s_client.list_namespaced_pod(
-        #     namespace=self.pod.metadata.namespace,
-        #     field_selector=f'metadata.name={self.pod.metadata.name}',
-        #     limit=1,
-        # )
-        # if pod.items:
-        #     k8s_client.delete_namespaced_pod(
-        #         name=self.pod.metadata.name,
-        #         namespace=self.pod.metadata.namespace,
-        #     )
+        pod = k8s_client.list_namespaced_pod(
+            namespace=self.pod.metadata.namespace,
+            field_selector=f'metadata.name={self.pod.metadata.name}',
+            limit=1,
+        )
+        if pod.items:
+            k8s_client.delete_namespaced_pod(
+                name=self.pod.metadata.name,
+                namespace=self.pod.metadata.namespace,
+            )
 
         # Fail task if driver pod failed
-        # if driver_pod.items[0].status.phase != 'Succeeded':
-        #     if env in self.skip_fail_env:
-        #         raise AirflowSkipException()
-        #     else:
-        #         raise AirflowFailException('Spark job failed')
+        if driver_pod.items[0].status.phase != 'Succeeded':
+            if env in self.skip_fail_env:
+                raise AirflowSkipException()
+            else:
+                raise AirflowFailException('Spark job failed')
