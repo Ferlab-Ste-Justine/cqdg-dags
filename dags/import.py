fe8ef859779b1b67e9b00def9104b6512997b1a8
adrian <adipaul1981@yahoo.ca>
2023-03-08 12:20:09 -0500

    test

--- dags/import.py
+++ dags/import.py
@@ -8,7 +8,7 @@ from lib.config import env, Env, K8sContext
 from lib.operators.spark import SparkOperator
 from lib.operators.arranger import ArrangerOperator
 
-# 4
+# 6
 
 with DAG(
         dag_id='import',
