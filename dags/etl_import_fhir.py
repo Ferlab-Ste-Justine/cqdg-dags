f108519f0f2389d2deac32b2d8b615cf19b34891
adrian <adipaul1981@yahoo.ca>
2023-04-05 09:26:46 -0400

    fix: CQDG-00 pass parmans in etl_import_fhir - fix default values

--- dags/etl_import_fhir.py
+++ dags/etl_import_fhir.py
@@ -14,10 +14,10 @@ with DAG(
     schedule_interval=None,
     params={
         'prefix': Param('clinical-data/e2adb961-4f58-4e13-a24f-6725df802e2c', type='string'),
-        'bucket': Param('ST0000017', type='string'),
-        'version': Param('cqdg', type='string'),
-        'release': Param('9200', type='string'),
-        'study': Param('9200', type='string'),
+        'bucket': Param('cqdg-qa-app-clinical-data-service', type='string'),
+        'version': Param('7', type='string'),
+        'release': Param('13', type='string'),
+        'study': Param('cag', type='string'),
         'run_names': Param('1615,1616,1617,1644,1645,1646,1647,1650,1651,1656,1658,1659,1660,1661,1667,1668,1669,1680,'
                            '1681,1682,1690,1691,1692,1704,1707,1735,1736,1737,1738,1748,1750,1854,1855,1856,1857,1860,'
                            '1861,1862,1865,1866,1867,1872,1873,1876,1877,1881,1882,1884,1885,1886,1887,1894,1895,1898,'
