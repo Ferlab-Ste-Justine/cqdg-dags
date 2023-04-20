e95515b647d01f54639a9b10dbb95de11ed58a16
adrian <adipaul1981@yahoo.ca>
2023-04-20 13:56:19 -0400

    feat: CQDG-141 narval

--- dags/etl_import_fhir.py
+++ dags/etl_import_fhir.py
@@ -12,6 +12,7 @@ with DAG(
     schedule_interval=None,
     params={
         'prefix': Param('clinical-data/e2adb961-4f58-4e13-a24f-6725df802e2c', type='string'),
+        'prefixFiles': Param('genorefq_wgs_data', type='string'),
         'bucket': Param('cqdg-qa-app-clinical-data-service', type='string'),
         'version': Param('7', type='string'),
         'release': Param('13', type='string'),
@@ -20,12 +21,16 @@ with DAG(
                            '1681,1682,1690,1691,1692,1704,1707,1735,1736,1737,1738,1748,1750,1854,1855,1856,1857,1860,'
                            '1861,1862,1865,1866,1867,1872,1873,1876,1877,1881,1882,1884,1885,1886,1887,1894,1895,1898,'
                            '1899,1902,1903,1906,1907,1916,1917,1918,1919,1920,1921,1922,1924,1925,1926,1927,1928,1929,'
-                           '1930,1934,1935,1951,1952,A00516_0270,A00516_0271', type='string')
+                           '1930,1934,1935,1951,1952,A00516_0270,A00516_0271', type='string'),
+        '_type': Param('nanuk', enum=['nanuk', 'narval']),
     },
 ) as dag:
 
     def prefix() -> str:
         return '{{ params.prefix }}'
+
+    def prefix_files() -> str:
+        return '{{ params.prefixFiles }}'
 
     def bucket() -> str:
         return '{{ params.bucket }}'
@@ -42,10 +47,13 @@ with DAG(
     def run_names() -> str:
         return '{{ params.run_names }}'
 
+    def _type() -> str:
+        return '{{ params._type }}'
+
     csv_import = FhirCsvOperator(
         task_id='fhir_import',
         name='etl-fhir_import',
         k8s_context=K8sContext.DEFAULT,
         arguments=["-cp", "cqdg-fhir-import.jar", "bio/ferlab/cqdg/etl/FhirImport",
-                   prefix(), bucket(), version(), release(), study(), "true", run_names()],
+                   prefix(), prefix_files(), bucket(), version(), release(), study(), "true", run_names(), _type()],
     )
