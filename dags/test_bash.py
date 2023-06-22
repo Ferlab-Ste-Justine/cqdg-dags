6027acf365d024885d0d111832adfcbaea0c30f7
Adrian <adipaul1981@yahoo.ca>
2023-06-22 12:06:10 -0400

    test bash script

---
+++ dags/test_bash.py
@@ -0,0 +1,15 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.operators.bash import BashOperator
+
+with DAG(
+        dag_id='test_bash',
+        start_date=datetime(2022, 1, 1),
+        schedule_interval=None,
+) as dag:
+
+
+    test_bash = BashOperator(
+        task_id='test_bash',
+        bash_command="utils/es_mapping_import.sh"
+    )
