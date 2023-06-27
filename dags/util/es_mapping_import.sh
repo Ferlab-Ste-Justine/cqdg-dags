61fdba39f3a51439330373434a7e51f210c1eb2b
Adrian <adipaul1981@yahoo.ca>
2023-06-27 10:37:37 -0400

    test

--- dags/util/es_mapping_import.sh
+++ dags/util/es_mapping_import.sh
@@ -10,4 +10,6 @@ export PATH=$PATH:$HOME/minio-binaries/
 
 mc cp https://raw.githubusercontent.com/Ferlab-Ste-Justine/etl-cqdg-portal/master/index-task/src/main/resources/templates/template_study_centric.json https://s3.ops.cqdg.ferlab.bio/minio/cqdg-qa-app-clinical-data-service/templates/template_study_centric.json
 
+mc ls
+
 mc --help
