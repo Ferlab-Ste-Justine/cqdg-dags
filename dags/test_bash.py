c5599e87e4fb1eab80d8c2ecbf544d0bec9701a6
Adrian <adipaul1981@yahoo.ca>
2023-06-28 14:30:30 -0400

    test

--- dags/test_bash.py
+++ dags/test_bash.py
@@ -1,16 +1,16 @@
 from airflow import DAG
 from datetime import datetime
-from airflow.operators.bash import BashOperator
+from lib.operators.run_import_minio import FileImportOperator
+from lib.config import Env, K8sContext
 
-#2
 with DAG(
         dag_id='test_bash',
         start_date=datetime(2022, 1, 1),
         schedule_interval=None,
 ) as dag:
-
-
-    test_bash = BashOperator(
-        task_id='test_bash',
-        bash_command="util/es_mapping_import.sh"
+    test_bash = FileImportOperator(
+        task_id='fhavro_export',
+        name='etl-fhavro_export',
+        k8s_context=K8sContext.DEFAULT,
+        cmds=[f'util/es_mapping_import.sh $AWS_ENDPOINT $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY {Env}'],
     )
