36b51f272e5ebe1e3e0f5153999ba97d22df7caf
Adrian <adipaul1981@yahoo.ca>
2023-06-28 16:32:33 -0400

    test

--- dags/test_bash.py
+++ dags/test_bash.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from datetime import datetime
 from lib.operators.run_import_minio import FileImportOperator
 # from lib.operators.fhavro import
+from lib.config import env, Env, K8sContext
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from lib import config
 
@@ -22,6 +23,7 @@ with DAG(
         name='etl-fhavro_export',
         image="debian",
         is_delete_operator_pod=False,
+        k8s_context=K8sContext.DEFAULT,
         cmds=["bash", "-cx"],
         arguments=[toto],
     )
