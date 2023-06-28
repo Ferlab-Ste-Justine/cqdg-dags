f0ea92f51e6580b7c36a554570dbf15ef97dcb75
Adrian <adipaul1981@yahoo.ca>
2023-06-28 17:02:30 -0400

    test

--- dags/test_bash.py
+++ dags/test_bash.py
@@ -24,27 +24,13 @@ with DAG(
         echo Setting MC alias to this minio: $AWS_ENDPOINT
         
         mc --help
-        mc alias set myminio $1 $2 $3 $4
         
-        mkdir templates
-        
-        echo Downloading templates ...
-        wget  https://raw.githubusercontent.com/Ferlab-Ste-Justine/etl-cqdg-portal/master/index-task/src/main/resources/templates/template_study_centric.json -O ./templates/template_study_centric.json
-        wget  https://raw.githubusercontent.com/Ferlab-Ste-Justine/etl-cqdg-portal/master/index-task/src/main/resources/templates/template_file_centric.json -O ./templates/template_file_centric.json
-        wget  https://raw.githubusercontent.com/Ferlab-Ste-Justine/etl-cqdg-portal/master/index-task/src/main/resources/templates/template_participant_centric.json -O ./templates/template_participant_centric.json
-        wget  https://raw.githubusercontent.com/Ferlab-Ste-Justine/etl-cqdg-portal/master/index-task/src/main/resources/templates/template_biospecimen_centric.json -O ./templates/template_biospecimen_centric.json
-        
-        echo Copy templates ...
-        mc cp ./templates/template_study_centric.json myminio/cqdg-"$4"-app-clinical-data-service/templates/template_study_centric.json
-        mc cp ./templates/template_file_centric.json myminio/cqdg-"$4"-app-clinical-data-service/templates/template_file_centric.json
-        mc cp ./templates/template_participant_centric.json myminio/cqdg-"$4"-app-clinical-data-service/templates/template_participant_centric.json
-        mc cp ./templates/template_biospecimen_centric.json myminio/cqdg-"$4"-app-clinical-data-service/templates/template_biospecimen_centric.json
     """
 
     test_bash = KubernetesPodOperator(
         task_id='fhavro_export',
         name='etl-fhavro_export',
-        image="debian",
+        image="minio/mc",
         is_delete_operator_pod=False,
         cmds=["bash", "-cx"],
         arguments=[script],
