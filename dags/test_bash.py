df8762c527ca5d94f81cc97d6fffe9dd97794366
Adrian <adipaul1981@yahoo.ca>
2023-06-28 17:05:17 -0400

    test

--- dags/test_bash.py
+++ dags/test_bash.py
@@ -22,8 +22,12 @@ with DAG(
         export PATH=$PATH:$HOME/minio-binaries/
         
         echo Setting MC alias to this minio: $AWS_ENDPOINT
-        
-        mc --help
+        echo Downloading templates ...
+        wget  https://raw.githubusercontent.com/Ferlab-Ste-Justine/etl-cqdg-portal/master/index-task/src/main/resources/templates/template_study_centric.json -O ./templates/template_study_centric.json
+        wget  https://raw.githubusercontent.com/Ferlab-Ste-Justine/etl-cqdg-portal/master/index-task/src/main/resources/templates/template_file_centric.json -O ./templates/template_file_centric.json
+        wget  https://raw.githubusercontent.com/Ferlab-Ste-Justine/etl-cqdg-portal/master/index-task/src/main/resources/templates/template_participant_centric.json -O ./templates/template_participant_centric.json
+        wget  https://raw.githubusercontent.com/Ferlab-Ste-Justine/etl-cqdg-portal/master/index-task/src/main/resources/templates/template_biospecimen_centric.json -O ./templates/template_biospecimen_centric.json
+
         
     """
 
