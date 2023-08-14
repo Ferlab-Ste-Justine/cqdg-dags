69d3c7ad225f72504873d53cf7c1d3d1b13e83f6
Laura Bégin <lbegin@ferlab.bio>
2023-08-14 10:33:53 -0400

    fix: BG-23 Append dataset_ids to arguments at runtime (#14)

--- dags/create_tables.py
+++ dags/create_tables.py
@@ -6,7 +6,7 @@ from airflow.decorators import task
 from airflow.exceptions import ParamValidationError
 from airflow.models import Param, TaskInstance, DagRun
 
-from lib.config import K8sContext, variant_task_jar, default_config_file, default_params
+from lib.config import default_config_file, default_params, K8sContext, variant_task_jar
 from lib.operators.spark import SparkOperator
 
 # Update default params
@@ -24,32 +24,46 @@ with DAG(
         dag_id='create_tables',
         start_date=datetime(2022, 1, 1),
         schedule_interval=None,
-        params=params
+        params=params,
 ) as dag:
-    @task(task_id='get_args')
-    def get_args(**kwargs) -> List[str]:
+    @task(task_id='get_dataset_ids')
+    def get_dataset_ids(**kwargs) -> List[str]:
         ti: TaskInstance = kwargs['ti']
         dag_run: DagRun = ti.dag_run
 
         if 'dataset_ids' not in dag_run.conf or dag_run.conf['dataset_ids'] == []:
             raise ParamValidationError('Dag param "dataset_ids" is required')
 
-        args: List[str] = [
-            '--config', default_config_file,
-            '--steps', 'default',
-            '--app-name', 'create_table_and_view'
-        ]
+        dataset_ids_args = []
         dataset_ids = dag_run.conf['dataset_ids']
-        [args.extend(['--dataset_id', d]) for d in dataset_ids]
-        return args
+        [dataset_ids_args.extend(['--dataset_id', d]) for d in dataset_ids]
+        return dataset_ids_args
 
 
-    SparkOperator(
-        task_id='create_table_and_view',
-        name='create-table-and-view',
-        k8s_context=K8sContext.DEFAULT,
-        spark_jar=variant_task_jar,
-        spark_class='bio.ferlab.datalake.spark3.hive.CreateTableAndView',
-        spark_config='etl-index-task',
-        arguments=get_args()
-    )
+    class CreateTableAndView(SparkOperator):
+        template_fields = SparkOperator.template_fields + ('arguments', 'dataset_ids',)
+
+        def __init__(self,
+                     dataset_ids,
+                     **kwargs):
+            super().__init__(**kwargs)
+            self.dataset_ids = dataset_ids
+
+        def execute(self, **kwargs):
+            # Append dataset_ids to arguments at runtime, after dataset_ids has been templated. Otherwise, dataset_ids
+            # is interpreted as XComArg and can't be appended to arguments.
+            self.arguments = self.arguments + self.dataset_ids
+            super().execute(**kwargs)
+
+
+    CreateTableAndView(task_id='create_table_and_view',
+                       name='create-table-and-view',
+                       k8s_context=K8sContext.DEFAULT,
+                       spark_jar=variant_task_jar,
+                       spark_class='bio.ferlab.datalake.spark3.hive.CreateTableAndView',
+                       spark_config='etl-index-task',
+                       arguments=['--config', default_config_file,
+                                  '--steps', 'default',
+                                  '--app-name', 'create_table_and_view',
+                                  ],
+                       dataset_ids=get_dataset_ids())
