6ce2f0b0cad1896f6f6bebbde3e237ad1434994c
Adrian <adipaul1981@yahoo.ca>
2023-09-28 13:25:31 -0400

    fix: CQDG-00 revert delete pod

--- dags/lib/operators/fhavro.py
+++ dags/lib/operators/fhavro.py
@@ -11,7 +11,7 @@ class FhavroOperator(KubernetesPodOperator):
         **kwargs,
     ) -> None:
         super().__init__(
-            is_delete_operator_pod=False,
+            is_delete_operator_pod=True,
             in_cluster=config.k8s_in_cluster(k8s_context),
             cluster_context=config.k8s_cluster_context(k8s_context),
             namespace=config.k8s_namespace,
