fc9907f31840bbf72850032cde730a0bc99540b9
jecos <jcostanza@ferlab.bio>
2024-03-01 07:59:59 -0500

    feat: CQDG-642 Try airflow variables to update jar version automatically

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -4,13 +4,18 @@ from datetime import datetime
 from lib import config
 from lib.config import env, Env, K8sContext
 from lib.operators.spark import SparkOperator
-from airflow.operators.bash import BashOperator
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from airflow.models import Variable
 with DAG(
         dag_id='test_dag',
         start_date=datetime(2022, 1, 1),
         schedule_interval=None
 ) as dag:
-    run_this = BashOperator(
-        task_id="run_after_loop",
-        bash_command="echo {{ var.value.hello}}",
-    )
+    run_this = KubernetesPodOperator(
+    task_id="test_error_message",
+    image=Variable.get("hello"),
+    cmds=["/bin/sh"],
+    arguments=["-c", "echo {{ var.value.hello}}"],
+    name="test-config-map"
+)
+
